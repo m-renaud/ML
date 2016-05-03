1510cb7d5f09d2dbf4764f1aa2c93b49a8073a63
Matt Renaud <mrenaud92@gmail.com>
2016-05-02 23:33:55 -0700

    Rewrite Layer using matrix representation.
    Remove the 'Neuron' construct from the library. A layer is now
    represented as an nx1 vector of biases and nxm matrix of weights where n
    is the number of neurons in the layer and m is the number of inputs to
    the layer.

--- src/NN.hs
+++ src/NN.hs
@@ -1,20 +1,21 @@
 {-# LANGUAGE BangPatterns #-}
+{- |
+Neural network implementation.
+-}
 module NN
        (
          -- * Data types
          ActivationFunction
-       , Neuron
-       , Layer
-       , Network
+       , Neuron(..)
+       , Layer(..)
+       , Network(..)
 
          -- * Network initialization
-       , randNeuron
        , randLayer
        , randNetwork
 
          -- * Running networks
        , feedForward
-       , runLayer
        , runNetwork
 
          -- * Activation functions
@@ -24,8 +25,10 @@ module NN
 import Control.Monad (forM, replicateM)
 import Control.Monad.Random (Rand, liftRand)
 import Data.Random.Normal (normal)
-import Numeric.LinearAlgebra (R, Vector, dot, vector)
+import Numeric.LinearAlgebra (Matrix, R, Vector, (><), (#>), vector)
+import Numeric.LinearAlgebra.Extended (vectorize)
 import System.Random (RandomGen)
+
 
 -- ==================================================
 -- Types.
@@ -37,40 +40,43 @@ type ActivationFunction = R -> R
 -- | A Neuron has a bias and weights for each of its inputs.
 data Neuron = Neuron !R (Vector R) deriving Show
 
--- | A layer in a neural network is a list of Neurons with weights
---  for the outputs of the neurons from the previous level.
-data Layer = Layer [Neuron] deriving Show
+-- | A layer in a neural network is a bias vector and weight matrix.
+--
+-- Let /n/ be the number of neurons in the layer and /m/ be the number
+-- of inputs to the layer. Then 'layerBiases' is a vector in Rⁿ and
+-- 'layerWeights' is an /nxm/ real matrix.
+data Layer = Layer
+             { layerBiases  :: Vector R  -- ^ A vector in Rⁿ representing the neuron biases.
+             , layerWeights :: Matrix R  -- ^ An /nxm/ real matrix of the neuron weights.
+             } deriving Show
 
 -- | A network is a list of layers.
-data Network = Network [Layer] deriving Show
+data Network = Network
+               { networkLayers :: [Layer] -- ^ The layers in the neural network.
+               } deriving Show
 
 
 -- ==================================================
 -- Network initialization.
 -- ==================================================
 
--- | Generate a random neuron given the number of inputs.
---
--- The bias and weights are taken from a normal distribution with
--- mean 0 and standard deviation of 1.
-randNeuron :: RandomGen g => Int -> Rand g Neuron
-randNeuron numInputs = do
-    bias    <- liftRand normal
-    weights <- vector <$> replicateM numInputs (liftRand normal)
-    return $ Neuron bias weights
-
--- | Generate a random layer in the neural network given the number of inputs and neurons.
-randLayer :: RandomGen g => Int -> Int -> Rand g Layer
-randLayer numInputs numNeurons =
-    Layer <$> replicateM numNeurons (randNeuron numInputs)
-
+-- | Generate a randomly initialized layer in a neural network.
+randLayer :: RandomGen g
+             => Int  -- ^ The number of inputs into the layer.
+             -> Int  -- ^ The number of neurons in the layer.
+             -> Rand g Layer
+randLayer numInputs numNeurons = do
+    bias <- vector <$> replicateM numNeurons (liftRand normal)
+    weights <- (numNeurons><numInputs) <$> replicateM (numNeurons*numInputs) (liftRand normal)
+    return $ Layer bias weights
 
 -- | Generate a random neural network given the sizes of each of the layers.
 --
--- 'sizes' is a list of the number of neurons in each layer. For example, [3,2,4]
--- will generate a neural network with 3 input neurons, 1 hidden layer with 2 neurons
--- and 4 output neurons.
-randNetwork :: RandomGen g => [Int] -> Rand g Network
+-- For example, [3,2,4] will generate a neural network with 3 input
+-- neurons, 1 hidden layer with 2 neurons and 4 output neurons.
+randNetwork :: RandomGen g
+               => [Int]  -- ^ The number of neurons in each layer.
+               -> Rand g Network
 randNetwork sizes = Network <$> forM dims (uncurry randLayer)
     where dims = adjacentPairs sizes
 
@@ -79,29 +85,30 @@ randNetwork sizes = Network <$> forM dims (uncurry randLayer)
 -- Example: adjacentPairs [1,2,3] ==> [(1,2), (2,3)]
 adjacentPairs :: [a] -> [(a,a)]
 adjacentPairs xs = zip (init xs) (tail xs)
-    
+
+
 -- ==================================================
 -- Running networks.
--- ==================================================  
+-- ==================================================
 
--- | Feed the output from the previous neuron layer into another neuron.
-feedForward :: ActivationFunction -> Vector R -> Neuron -> R
-feedForward act x (Neuron bias w) = act z
-    where z = (w `dot` x) + bias
-
--- | Run a neural network layer given input 'x'.
-runLayer :: ActivationFunction -> Vector R -> Layer -> Vector R
-runLayer act x (Layer neurons) = vector $ map (feedForward act x) neurons
+-- | Feed the output from a previous layer to the next layer.
+feedForward :: ActivationFunction  -- ^ Neuron activation function.
+               -> Vector R         -- ^ Input to the layer.
+               -> Layer            -- ^ The layer to run.
+               -> Vector R
+feedForward act x (Layer biases weights) = (vectorize act) z
+    where z = (weights #> x) + biases
 
 -- | Run a neural network with the input vector 'x'.
 runNetwork :: Network -> ActivationFunction -> Vector R -> Vector R
 runNetwork (Network []) _act x = x
-runNetwork (Network (l:ls)) act input = runNetwork (Network ls) act (runLayer act input l)
+runNetwork (Network (l:ls)) act input = runNetwork (Network ls) act (feedForward act input l)
 
 
 -- ==================================================
 -- Activation functions.
--- ==================================================  
-  
+-- ==================================================
+
+-- | The sigmoid function.
 sigmoid :: ActivationFunction
 sigmoid !z = 1.0 / (1.0 + exp (-z))
